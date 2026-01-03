16edba8de7ddfc1b29b5dc6571de5a24c0df7172
consultedge <sammyceg@gmail.com>
2026-01-03 21:45:49 +0530

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,6 +1,4 @@
-const config = {
+window._workshopConfig = {
   apiEndpoint: "https://cjbj03sjbl.execute-api.ap-south-1.amazonaws.com/edit-image",
   region: "ap-south-1"
 };
-
-export default config;
