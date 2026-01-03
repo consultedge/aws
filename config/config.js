ef29e97a4f89cf695c57b8e3d43d1719f6889848
consultedge <sammyceg@gmail.com>
2026-01-03 21:54:14 +0530

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,4 +1,16 @@
+// AWS Workshop Configuration File
+// AI for Bharat – Image Editing Workshop
+
 window._workshopConfig = {
-  apiEndpoint: "https://cjbj03sjbl.execute-api.ap-south-1.amazonaws.com/edit-image",
-  region: "ap-south-1"
+  cognito: {
+    userPoolId: 'ap-south-1_2rJUAB5Mo',
+    userPoolClientId: '5s6o5grmnkp994ft9kepja0ge7',
+    region: 'ap-south-1'
+  },
+  api: {
+    invokeUrl: 'https://cjbj03sjbl.execute-api.ap-south-1.amazonaws.com'
+  }
 };
+
+// Configuration validation flag
+window._configLoaded = true;
